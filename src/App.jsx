29f69cf55de5a8095bbea21a19bf616f8f2057f1
Sai Aryan Goswami <saiaryan.goswami1784@gmail.com>
2023-12-01 11:53:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame} from '@react-three/fiber'
+import './App.css'
+
+
+const App = () => {
+  return <Canvas>
+
+  </Canvas>
+}
+
+export default App
